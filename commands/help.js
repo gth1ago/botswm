6208eededd6daa525201afc644837506ac97d045
Yuri Alves <yuripiresalves@gmail.com>
2021-11-12 19:52:32 -0300

    update

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ajuda')
-    .setDescription('Sobre o bot'),
+    .setDescription('Sobre o bot!'),
   async execute(interaction) {
     await interaction.reply(':robot: Olá! Ainda estou em construção :wrench:');
   },
